c144bafbd414b5d93bd3a9aca097beb24ac19d26
fahadalduaij <fahad_osama@hotmail.com>
2021-11-22 19:22:37 +0300

    from fahad d

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../Home";
 import Signin from "../Authentication/Signin";
@@ -6,19 +6,22 @@ import Signup from "../Authentication/Signup";
 import timeline from "../screens/timeline";
 import UserProfileMain from "../Profile/UserProfileMain";
 import Search from "../screens/Search";
+import authStore from "../../stores/authStore";
+import { observer } from "mobx-react-lite";
 
 const RootNavigator = () => {
-  const { Navigator, Screen } = createStackNavigator();
-  return (
-    <Navigator initialRouteName="Home">
-      <Screen name="Home" component={Home} options={{}} />
-      <Screen name="Signin" component={Signin} />
-      <Screen name="Signup" component={Signup} />
-      <Screen name="timeline" component={timeline} />
-      <Screen name="UserProfileMain" component={UserProfileMain} />
-      <Screen name="Search" component={Search} />
-    </Navigator>
-  );
+	const { Navigator, Screen } = createStackNavigator();
+
+	return (
+		<Navigator initialRouteName={authStore.user ? "timeline" : "Home"}>
+			<Screen name="Home" component={Home} options={{}} />
+			<Screen name="Signin" component={Signin} />
+			<Screen name="Signup" component={Signup} />
+			<Screen name="timeline" component={timeline} />
+			<Screen name="UserProfileMain" component={UserProfileMain} />
+			<Screen name="Search" component={Search} />
+		</Navigator>
+	);
 };
 
-export default RootNavigator;
+export default observer(RootNavigator);
--- stores/instance.js
+++ stores/instance.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
-export const baseURL = "http://192.168.8.101:8000"; //AlKhaldi
-// export const baseURL = "http://192.168.1.75:8000";"" //alduaij
-// "192.168.8.101" alaskar
+//AlKhaldi
+// export const baseURL = "http://192.168.8.101:8000";
+
+//alduaij
+export const baseURL = "http://192.168.1.75:8000";
+
+//alaskar
+// export const baseURL = "http://192.168.8.101:8000";
 
 export const instance = axios.create({
-  baseURL: `${baseURL}/api`,
+	baseURL: `${baseURL}/api`,
 });
