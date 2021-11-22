ccbeddbc629ee37b6c51e8e43cf28575013f935c
ahmad <aealaskar@gmail.com>
2021-11-22 18:33:38 +0300

    edit

--- stores/instance.js
+++ stores/instance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-export const baseURL = "http://192.168.8.128:8000"; //AlKhaldi
+export const baseURL = "http://192.168.8.101:8000"; //AlKhaldi
 // export const baseURL = "http://192.168.1.75:8000";"" //alduaij
+// "192.168.8.101" alaskar
 
 export const instance = axios.create({
   baseURL: `${baseURL}/api`,
