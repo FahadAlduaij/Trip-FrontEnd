65e9956a95be1477c57d6278c7f58576cd5a2f09
ahmad <aealaskar@gmail.com>
2021-11-23 19:15:46 +0300

    timeline need more items to test style

--- components/Trips/TripItem.js
+++ components/Trips/TripItem.js
@@ -20,54 +20,55 @@ const TripItem = ({ trip, navigation, route }) => {
       onPress={() => navigation.navigate("TripDetail", { trip: trip })}
     >
       <SafeAreaView style={styles.container}>
-        <ScrollView style={styles.scrollView}>
-          <Box
-            h="300"
-            marginBottom="5"
-            maxW="80"
-            rounded="lg"
-            overflow="hidden"
-            borderColor="coolGray.200"
-            borderWidth="1"
-            _dark={{
-              borderColor: "coolGray.600",
-              backgroundColor: "gray.700",
-            }}
-            _web={{
-              shadow: 2,
-              borderWidth: 0,
-            }}
-            _light={{
-              backgroundColor: "gray.50",
-            }}
-          >
-            <Box>
-              <AspectRatio w="100%" ratio={16 / 9}>
-                <Image
-                  source={{
-                    uri: trip.image,
-                  }}
-                  alt="image"
-                />
-              </AspectRatio>
-            </Box>
-            <Stack p="4" space={3}>
-              <Stack space={2}>
-                <Heading size="md" ml="-1">
-                  {trip.title}
-                </Heading>
-              </Stack>
-
-              <HStack
-                alignItems="center"
-                space={4}
-                justifyContent="space-between"
-              >
-                <HStack alignItems="center"></HStack>
-              </HStack>
-            </Stack>
+        <Box
+          h="300"
+          marginBottom="5"
+          marginTop="150"
+          marginLeft="10"
+          marginRight="10"
+          maxW="80"
+          rounded="lg"
+          overflow="hidden"
+          borderColor="coolGray.200"
+          borderWidth="1"
+          _dark={{
+            borderColor: "coolGray.600",
+            backgroundColor: "gray.700",
+          }}
+          _web={{
+            shadow: 2,
+            borderWidth: 0,
+          }}
+          _light={{
+            backgroundColor: "gray.50",
+          }}
+        >
+          <Box>
+            <AspectRatio w="100%" ratio={16 / 9}>
+              <Image
+                source={{
+                  uri: trip.image,
+                }}
+                alt="image"
+              />
+            </AspectRatio>
           </Box>
-        </ScrollView>
+          <Stack p="4" space={3}>
+            <Stack space={2}>
+              <Heading size="md" ml="-1">
+                {trip.title}
+              </Heading>
+            </Stack>
+
+            <HStack
+              alignItems="center"
+              space={4}
+              justifyContent="space-between"
+            >
+              <HStack alignItems="center"></HStack>
+            </HStack>
+          </Stack>
+        </Box>
       </SafeAreaView>
     </Pressable>
   );
@@ -82,9 +83,5 @@ const styles = StyleSheet.create({
   },
   scrollView: {
     marginHorizontal: 20,
-  },
-  card: {
-    flex: 1,
-    height: "100%",
   },
 });
