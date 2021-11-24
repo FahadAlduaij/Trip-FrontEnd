5595447c33904524c1ffe615132b9368bee3287a
FAHADKHALDI <fahadalkhaldi96@gmail.com>
2021-11-24 23:14:48 +0300

    some changes

--- components/Profile/EditProfileModal.js
+++ components/Profile/EditProfileModal.js
@@ -1,94 +1,103 @@
 import React, { useState } from "react";
-import { Button, Modal, FormControl, Input, Icon } from "native-base";
+import { Modal, FormControl, Input, Icon, View } from "native-base";
 import * as ImagePicker from "expo-image-picker";
 import { Ionicons } from "@expo/vector-icons";
 import { observer } from "mobx-react";
+import { Button } from "react-native-elements";
 
 // Stores
 import authStore from "../../stores/authStore";
 
 const EditProfileModal = () => {
-	const [update, setUpdate] = useState(authStore.user);
+  const [update, setUpdate] = useState(authStore.user);
 
-	const [showModal, setShowModal] = useState(false);
-	const _pickImage = async () => {
-		try {
-			let result = await ImagePicker.launchImageLibraryAsync({
-				mediaTypes: ImagePicker.MediaTypeOptions.All,
-				allowsEditing: true,
-				aspect: [4, 3],
+  const [showModal, setShowModal] = useState(false);
+  const _pickImage = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
 
-				quality: 1,
-			});
+        quality: 1,
+      });
 
-			if (!result.cancelled) {
-				const localUri = result.uri;
-				const filename = localUri.split("/").pop();
-				const match = /\.(\w+)$/.exec(filename);
-				const image = {
-					uri: localUri,
-					name: filename,
-					type: match ? `image/${match[1]}` : `image`,
-				};
-				setUpdate({ ...update, image });
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
-	const handleSubmit = () => {
-		authStore.editUser(update);
-		setShowModal(false);
-	};
+      if (!result.cancelled) {
+        const localUri = result.uri;
+        const filename = localUri.split("/").pop();
+        const match = /\.(\w+)$/.exec(filename);
+        const image = {
+          uri: localUri,
+          name: filename,
+          type: match ? `image/${match[1]}` : `image`,
+        };
+        setUpdate({ ...update, image });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleSubmit = () => {
+    authStore.editUser(update);
+    setShowModal(false);
+  };
 
-	return (
-		<>
-			<Button onPress={() => setShowModal(true)}>Edit Profile</Button>
-			<Modal isOpen={showModal} onClose={() => setShowModal(false)}>
-				<Modal.Content maxWidth="400px">
-					<Modal.CloseButton />
-					<Modal.Header>Edit Profile</Modal.Header>
-					<Modal.Body>
-						<FormControl>
-							<FormControl.Label>Edit Name</FormControl.Label>
-							<Input onChangeText={(name) => setUpdate({ ...update, name })} />
-						</FormControl>
-						<FormControl mt="3">
-							<FormControl.Label>Edit Bio</FormControl.Label>
-							<Input onChangeText={(bio) => setUpdate({ ...update, bio })} />
-						</FormControl>
-						<FormControl mt="3">
-							<FormControl.Label>Change Image</FormControl.Label>
-							<Button
-								onPress={_pickImage}
-								variant="outline"
-								colorScheme="coolGray"
-								leftIcon={
-									<Icon as={Ionicons} name="cloud-upload-outline" size="sm" />
-								}
-							>
-								Upload Image
-							</Button>
-						</FormControl>
-					</Modal.Body>
-					<Modal.Footer>
-						<Button.Group space={2}>
-							<Button
-								variant="ghost"
-								colorScheme="blueGray"
-								onPress={() => {
-									setShowModal(false);
-								}}
-							>
-								Cancel
-							</Button>
-							<Button onPress={handleSubmit}>Save</Button>
-						</Button.Group>
-					</Modal.Footer>
-				</Modal.Content>
-			</Modal>
-		</>
-	);
+  return (
+    <>
+      <Button
+        onPress={() => setShowModal(true)}
+        Style={{
+          backgroundColor: "#2f8ce3",
+        }}
+        containerStyle={{ width: "82%", marginTop: 10 }}
+      >
+        Edit Profile
+      </Button>
+      <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+        <Modal.Content maxWidth="400px">
+          <Modal.CloseButton />
+          <Modal.Header>Edit Profile</Modal.Header>
+          <Modal.Body>
+            <FormControl>
+              <FormControl.Label>Edit Name</FormControl.Label>
+              <Input onChangeText={(name) => setUpdate({ ...update, name })} />
+            </FormControl>
+            <FormControl mt="3">
+              <FormControl.Label>Edit Bio</FormControl.Label>
+              <Input onChangeText={(bio) => setUpdate({ ...update, bio })} />
+            </FormControl>
+            <FormControl mt="3">
+              <FormControl.Label>Change Image</FormControl.Label>
+              <Button
+                onPress={_pickImage}
+                variant="outline"
+                colorScheme="coolGray"
+                leftIcon={
+                  <Icon as={Ionicons} name="cloud-upload-outline" size="sm" />
+                }
+              >
+                Upload Image
+              </Button>
+            </FormControl>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button.Group space={2}>
+              <Button
+                variant="ghost"
+                colorScheme="blueGray"
+                onPress={() => {
+                  setShowModal(false);
+                }}
+              >
+                Cancel
+              </Button>
+              <Button onPress={handleSubmit}>Save</Button>
+            </Button.Group>
+          </Modal.Footer>
+        </Modal.Content>
+      </Modal>
+    </>
+  );
 };
 
 export default observer(EditProfileModal);
--- components/Trips/TripDetail.js
+++ components/Trips/TripDetail.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 import {
-	Text,
-	View,
-	Image,
-	ScrollView,
-	HStack,
-	Center,
-	useToast,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  HStack,
+  Center,
+  useToast,
+  Pressable,
 } from "native-base";
-import { Button } from "react-native-elements";
+import { Button, Avatar } from "react-native-elements";
 import { observer } from "mobx-react";
 
 // Stores
@@ -18,173 +19,204 @@ import tripStore from "../../stores/tripStore";
 import EditTripModal from "./EditTripModal";
 
 const TripDetail = ({ route, navigation }) => {
-	const trip = route.params.trip;
-	const toast = useToast();
+  const trip = route.params.trip;
+  const user = trip.owner;
+  const toast = useToast();
 
-	const handleDelete = () => {
-		tripStore.deleteTrip(trip, toast, navigation);
-	};
+  const handleDelete = () => {
+    tripStore.deleteTrip(trip, toast, navigation);
+  };
 
-	return (
-		<View style={styles.container}>
-			<ScrollView>
-				<View style={{ alignItems: "center", marginHorizontal: 30 }}>
-					<Image
-						style={styles.tripImg}
-						source={{ uri: baseURL + trip.image }}
-						alt="trip image description"
-					/>
-					<Text style={styles.name}>{trip.title}</Text>
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <Pressable
+          onPress={() => navigation.navigate("ProfilePage", { trip: trip })}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              padding: 10,
+              paddingLeft: 15,
+              borderTopColor: "black",
+              borderTopWidth: 0.5,
+              backgroundColor: "transparent",
+              marginTop: 20,
+            }}
+          >
+            <Avatar
+              size="medium"
+              rounded
+              source={{ uri: baseURL + user.image }}
+            />
 
-					<Text style={styles.description}>{trip.description}</Text>
-				</View>
-				<View style={styles.starContainer}>
-					<Image
-						style={styles.star}
-						source={{
-							uri: "https://img.icons8.com/color/40/000000/star.png",
-						}}
-						alt="Start 1"
-					/>
-					<Image
-						style={styles.star}
-						source={{
-							uri: "https://img.icons8.com/color/40/000000/star.png",
-						}}
-						alt="Start 2"
-					/>
-					<Image
-						style={styles.star}
-						source={{
-							uri: "https://img.icons8.com/color/40/000000/star.png",
-						}}
-						alt="Start 3"
-					/>
-					<Image
-						style={styles.star}
-						source={{
-							uri: "https://img.icons8.com/color/40/000000/star.png",
-						}}
-						alt="Start 4"
-					/>
-					<Image
-						style={styles.star}
-						source={{
-							uri: "https://img.icons8.com/color/40/000000/star.png",
-						}}
-						alt="Start 5"
-					/>
-				</View>
-				<View style={styles.addToListContainer}>
-					<Button
-						title="Add to my trips list"
-						buttonStyle={{
-							backgroundColor: "#2f8ce3",
-							alignItems: "center",
-						}}
-						containerStyle={{ width: "82%", marginTop: 10 }}
-					/>
-				</View>
-				<Center>
-					<HStack mt="5">
-						<EditTripModal trip={trip} />
+            <View style={{ marginLeft: 10, marginBottom: 5 }}>
+              <Text
+                style={{ fontSize: 16, fontWeight: "bold", color: "white" }}
+              >
+                {user.name}
+              </Text>
+            </View>
+          </View>
+        </Pressable>
+        <View style={{ alignItems: "center", marginHorizontal: 30 }}>
+          <Image
+            style={styles.tripImg}
+            source={{ uri: baseURL + trip.image }}
+            alt="trip image description"
+          />
+          <Text style={styles.name}>{trip.title}</Text>
 
-						<Button
-							title="Delete"
-							buttonStyle={{
-								backgroundColor: "#dc2626",
-								margin: 5,
-								width: 100,
-							}}
-							onPress={handleDelete}
-						/>
-					</HStack>
-				</Center>
-			</ScrollView>
-		</View>
-	);
+          <Text style={styles.description}>{trip.description}</Text>
+        </View>
+        <View style={styles.starContainer}>
+          <Image
+            style={styles.star}
+            source={{
+              uri: "https://img.icons8.com/color/40/000000/star.png",
+            }}
+            alt="Start 1"
+          />
+          <Image
+            style={styles.star}
+            source={{
+              uri: "https://img.icons8.com/color/40/000000/star.png",
+            }}
+            alt="Start 2"
+          />
+          <Image
+            style={styles.star}
+            source={{
+              uri: "https://img.icons8.com/color/40/000000/star.png",
+            }}
+            alt="Start 3"
+          />
+          <Image
+            style={styles.star}
+            source={{
+              uri: "https://img.icons8.com/color/40/000000/star.png",
+            }}
+            alt="Start 4"
+          />
+          <Image
+            style={styles.star}
+            source={{
+              uri: "https://img.icons8.com/color/40/000000/star.png",
+            }}
+            alt="Start 5"
+          />
+        </View>
+        <View style={styles.addToListContainer}>
+          <Button
+            title="Add to my trips list"
+            buttonStyle={{
+              backgroundColor: "#2f8ce3",
+              alignItems: "center",
+            }}
+            containerStyle={{ width: "82%", marginTop: 10 }}
+          />
+        </View>
+        <Center>
+          <HStack mt="5">
+            <EditTripModal trip={trip} />
+
+            <Button
+              title="Delete"
+              buttonStyle={{
+                backgroundColor: "#dc2626",
+                margin: 5,
+                width: 100,
+              }}
+              onPress={handleDelete}
+            />
+          </HStack>
+        </Center>
+      </ScrollView>
+    </View>
+  );
 };
 
 export default observer(TripDetail);
 
 const styles = StyleSheet.create({
-	container: {
-		paddingTop: 100,
-		flex: 1,
-		backgroundColor: "#0f1010",
-	},
-	tripImg: {
-		width: 500,
-		height: 300,
-	},
-	name: {
-		fontSize: 20,
-		color: "#696969",
-		fontWeight: "bold",
-		marginTop: 30,
-		alignItems: "center",
-	},
-	description: {
-		textAlign: "center",
-		marginTop: 10,
-		color: "#696969",
-	},
-	star: {
-		width: 40,
-		height: 40,
-	},
-	btnColor: {
-		height: 30,
-		width: 30,
-		borderRadius: 30,
-		marginHorizontal: 3,
-	},
-	btnSize: {
-		height: 40,
-		width: 40,
-		borderRadius: 40,
-		borderColor: "#778899",
-		borderWidth: 1,
-		marginHorizontal: 3,
-		backgroundColor: "white",
+  container: {
+    paddingTop: 100,
+    flex: 1,
+    backgroundColor: "#0f1010",
+  },
+  tripImg: {
+    width: 500,
+    height: 300,
+  },
+  name: {
+    fontSize: 20,
+    color: "#696969",
+    fontWeight: "bold",
+    marginTop: 30,
+    alignItems: "center",
+  },
+  description: {
+    textAlign: "center",
+    marginTop: 10,
+    color: "#696969",
+  },
+  star: {
+    width: 40,
+    height: 40,
+  },
+  btnColor: {
+    height: 30,
+    width: 30,
+    borderRadius: 30,
+    marginHorizontal: 3,
+  },
+  btnSize: {
+    height: 40,
+    width: 40,
+    borderRadius: 40,
+    borderColor: "#778899",
+    borderWidth: 1,
+    marginHorizontal: 3,
+    backgroundColor: "white",
 
-		flexDirection: "row",
-		justifyContent: "center",
-		alignItems: "center",
-	},
-	starContainer: {
-		justifyContent: "center",
-		marginHorizontal: 30,
-		flexDirection: "row",
-		marginTop: 20,
-	},
-	contentColors: {
-		justifyContent: "center",
-		marginHorizontal: 30,
-		flexDirection: "row",
-		marginTop: 20,
-	},
-	contentSize: {
-		justifyContent: "center",
-		marginHorizontal: 30,
-		flexDirection: "row",
-		marginTop: 20,
-	},
-	shareButton: {
-		marginTop: 10,
-		height: 45,
-		flexDirection: "row",
-		justifyContent: "center",
-		alignItems: "center",
-		borderRadius: 30,
-		backgroundColor: "#00BFFF",
-	},
-	shareButtonText: {
-		color: "#FFFFFF",
-		fontSize: 20,
-	},
-	addToListContainer: {
-		marginHorizontal: 30,
-		alignItems: "center",
-	},
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  starContainer: {
+    justifyContent: "center",
+    marginHorizontal: 30,
+    flexDirection: "row",
+    marginTop: 20,
+  },
+  contentColors: {
+    justifyContent: "center",
+    marginHorizontal: 30,
+    flexDirection: "row",
+    marginTop: 20,
+  },
+  contentSize: {
+    justifyContent: "center",
+    marginHorizontal: 30,
+    flexDirection: "row",
+    marginTop: 20,
+  },
+  shareButton: {
+    marginTop: 10,
+    height: 45,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 30,
+    backgroundColor: "#00BFFF",
+  },
+  shareButtonText: {
+    color: "#FFFFFF",
+    fontSize: 20,
+  },
+  addToListContainer: {
+    marginHorizontal: 30,
+    alignItems: "center",
+  },
 });
